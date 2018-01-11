449c30016d1eaf1cc8b2fe8815455bde629f3a85
Simon <sjosiek@hs-mittweida.de>
2018-01-11 21:36:20 +0100

    Random Book Covers; some Material stuff; new Font

--- Assets/Scripts/RandomBook.cs
+++ Assets/Scripts/RandomBook.cs
@@ -5,24 +5,11 @@ using UnityEngine;
 
 public class RandomBook : MonoBehaviour
 {
-    [SerializeField] private List<Texture2D> books;
-    private Texture2D[] atlasTextures;
-    private Rect[] rects;
+    [SerializeField] private Material[] books;
 
     private void Start()
     {
-        books = new List<Texture2D>(Resources.LoadAll<Texture2D>("Books"));
-
-        atlasTextures = books.ToArray();
-
-        Texture2D atlas = new Texture2D(512, 512);
-        rects = atlas.PackTextures(atlasTextures, 2, 512);
-
-        int x = Random.Range(0, 1);
-        int y = Random.Range(0, 1);
-        Material mat = new Material(GetComponent<Renderer>().sharedMaterial);
-        mat.SetTexture("_MainTex", atlas);        
-        mat.SetTextureOffset("_MainTex", new Vector2(x, y).normalized);
-        GetComponent<Renderer>().sharedMaterial = mat;
+        books = Resources.LoadAll<Material>("Books");
+        GetComponent<Renderer>().material = books[Random.Range(0, books.Length)];
     }
 }
